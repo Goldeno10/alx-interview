f8b6fab8c0555bb58fd52cdc673bbd9cc1b3cf6a
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-09-07 20:12:53 +0100

    complete prime number game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Task:
+    Maria and Ben are playing a game. Given a set of consecutive
+    integers starting from 1 up to and including n, they take turns
+    choosing a prime number from the set and removing that number and
+    its multiples from the set. The player that cannot make a move
+    loses the game.
+    They play x rounds of the game, where n may be different for each round.
+    Assuming Maria always goes first and both players play optimally,
+    determine who the winner of each game is.
+
+    Prototype: def isWinner(x, nums)
+    where x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    You cannot import any packages in this task
+    Example:
+
+    x = 3, nums = [4, 5, 1]
+    First round: 4
+
+    Maria picks 2 and removes 2, 4, leaving 1, 3
+    Ben picks 3 and removes 3, leaving 1
+    Ben wins because there are no prime numbers left for Maria to choose
+    Second round: 5
+
+    Maria picks 2 and removes 2, 4, leaving 1, 3, 5
+    Ben picks 3 and removes 3, leaving 1, 5
+    Maria picks 5 and removes 5, leaving 1
+    Maria wins because there are no prime numbers left for Ben to choose
+    Third round: 1
+
+    Ben wins because there are no prime numbers for Maria to choose
+    Result: Ben has the most wins
+"""
+
+
+def isWinner(x, nums):
+    """Determines the winner of each game"""
+    def is_prime(num):
+        """Checks if a number is prime"""
+        if num <= 1:
+            return False
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    def find_primes_up_to_n(n):
+        """Find all prin numbers in range of n"""
+        primes = []
+        for i in range(2, n + 1):
+            if is_prime(i):
+                primes.append(i)
+        return primes
+
+    max_n = max(nums)
+    primes = find_primes_up_to_n(max_n)
+    dp = [None] * (max_n + 1)
+    dp[0] = dp[1] = False
+
+    for i in range(2, max_n + 1):
+        if not dp[i]:
+            for prime in primes:
+                if i + prime <= max_n:
+                    dp[i + prime] = True
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if dp[n]:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
